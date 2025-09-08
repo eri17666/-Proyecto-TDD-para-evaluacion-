8145100a850e48de127de5994a8e03125d8057cf
ERICK1766 <guzmanquispeerickivan@gmail.com>
2025-09-08 19:11:38 -0400

    test: inicializar el test para caso base

---
+++ src/parqueo.test.js
@@ -0,0 +1,9 @@
+import calculateFee from "./parqueo.js";
+
+function mkDate(y, m, d, h, min) {
+  return new Date(y, m - 1, d, h, min, 0, 0);
+}
+describe("Parqueo - Caso base", () => {
+  it("30 minutos cobra 1 hora (Bs10)", () => {
+  });
+});
